cfc831ed31849036b048c0924525ea622581f1ca
notexplosive <thenotexplosive@gmail.com>
2022-08-27 12:11:42 -0700

    Remove Graphics.WindowSize

--- ExplogineMonoGame/Graphics.cs
+++ ExplogineMonoGame/Graphics.cs
@@ -23,7 +23,6 @@ public class Graphics
 
     public GraphicsDevice Device { get; }
     public Painter Painter { get; }
-    public Point WindowSize => Device.Viewport.Bounds.Size;
     public Point DisplaySize => new(Client.Graphics.Device.DisplayMode.Width, Client.Graphics.Device.DisplayMode.Height);
 
     public Texture2D CropTexture(Rectangle rect, Texture2D sourceTexture)
