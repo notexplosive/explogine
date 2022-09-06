71b32124b34c4799822f0435c2239d42deef4531
notexplosive <thenotexplosive@gmail.com>
2022-09-06 07:43:07 -0700

    Add comment

--- Library/ExplogineMonoGame/Cartridges/CartridgeConfig.cs
+++ Library/ExplogineMonoGame/Cartridges/CartridgeConfig.cs
@@ -4,6 +4,12 @@ namespace ExplogineMonoGame.Cartridges;
 
 public readonly struct CartridgeConfig
 {
+    /// <summary>
+    /// The desired locked resolution of this cartridge.
+    ///
+    /// If set to 1600x900, we will render the game to a 1600x900 canvas and letterbox the window where appropriate.
+    /// If set to null, we will stretch the rendered game canvas to be 1:1 with the window.
+    /// </summary>
     public Point? RenderResolution { get; }
 
     public CartridgeConfig(Point? renderResolution)
