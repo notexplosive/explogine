7210d2a1ed7151782134c927794c1ab60738bba5
notexplosive <thenotexplosive@gmail.com>
2022-12-01 09:23:08 -0800

    SeerLens behaves more like Painter; maybe it should just be part of Painter

--- Library/ExplogineMonoGame/Data/SeerLens.cs
+++ Library/ExplogineMonoGame/Data/SeerLens.cs
@@ -1,21 +1,34 @@
-﻿using System;
-using ExplogineMonoGame.AssetManagement;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace ExplogineMonoGame.Data;
 
 public class SeerLens
 {
-    public static void Render(Painter painter, RectangleF viewBounds, Point outputDimensions, Action<Painter> drawContent)
+    // this should go on RectangleF
+    public static Matrix RenderMatrix(RectangleF viewBounds, Point outputDimensions)
     {
-        var translation = 
+        var translation =
             Matrix.Invert(Matrix.CreateTranslation(new Vector3(viewBounds.Location, 0)));
-        var scale = 
-            Matrix.CreateScale(new Vector3(outputDimensions.X / viewBounds.Width, outputDimensions.Y / viewBounds.Height, 1));
-        painter.BeginSpriteBatch(SamplerState.LinearWrap, translation * scale);
+        return translation * SeerLens.RenderMatrixScalar(viewBounds, outputDimensions);
+    }
+
+    // this should go on RectangleF
+    public static Matrix RenderMatrixScalar(RectangleF viewBounds, Point outputDimensions)
+    {
+        return Matrix.CreateScale(new Vector3(outputDimensions.X / viewBounds.Width,
+            outputDimensions.Y / viewBounds.Height, 1));
+    }
+
+    // This should go on Painter
+    public static void Begin(Painter painter, RectangleF viewBounds, Point outputDimensions)
+    {
+        painter.BeginSpriteBatch(SamplerState.LinearWrap, SeerLens.RenderMatrix(viewBounds, outputDimensions));
         painter.Clear(Color.LightBlue);
-        drawContent(painter);
+    }
+
+    public static void End(Painter painter)
+    {
         painter.EndSpriteBatch();
     }
 }
