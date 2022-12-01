55e5fb1b16aec6d647d93b55e1eef3711dccae52
notexplosive <thenotexplosive@gmail.com>
2022-12-01 09:23:22 -0800

    Vector2Extensions supports StraightDivide

--- Library/ExplogineMonoGame/Data/Vector2Extensions.cs
+++ Library/ExplogineMonoGame/Data/Vector2Extensions.cs
@@ -16,7 +16,27 @@ public static class Vector2Extensions
         return new Vector2(vec.X * other.X, vec.Y * other.Y);
     }
     
+    public static Vector2 StraightMultiply(this Vector2 vec, float otherX, float otherY)
+    {
+        return new Vector2(vec.X * otherX, vec.Y * otherY);
+    }
+    
     public static Vector2 StraightMultiply(this Vector2 vec, Point other)
+    {
+        return vec.StraightMultiply(other.ToVector2());
+    }
+    
+    public static Vector2 StraightDivide(this Vector2 vec, Vector2 other)
+    {
+        return new Vector2(vec.X / other.X, vec.Y / other.Y);
+    }
+    
+    public static Vector2 StraightDivide(this Vector2 vec, float otherX, float otherY)
+    {
+        return new Vector2(vec.X / otherX, vec.Y / otherY);
+    }
+    
+    public static Vector2 StraightDivide(this Vector2 vec, Point other)
     {
         return vec.StraightMultiply(other.ToVector2());
     }
