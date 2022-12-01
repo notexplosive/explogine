f11833a24be25815d901462a623c767a066242c8
notexplosive <thenotexplosive@gmail.com>
2022-12-01 00:05:29 -0800

    Drag class

---
+++ Library/ExplogineMonoGame/Data/Drag.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ExplogineMonoGame.Data;
+
+public class Drag
+{
+    public bool IsDragging { get; private set; }
+    public Vector2 TotalDelta { get; private set; }
+    public Vector2 StartPosition { get; private set; }
+
+    public void Start(Vector2 startPosition)
+    {
+        IsDragging = true;
+        StartPosition = startPosition;
+    }
+
+    public void End()
+    {
+        IsDragging = false;
+        StartPosition = Vector2.Zero;
+        TotalDelta = Vector2.Zero;
+    }
+
+    public void AddDelta(Vector2 delta)
+    {
+        if (IsDragging)
+        {
+            TotalDelta += delta;
+        }
+    }
+}
