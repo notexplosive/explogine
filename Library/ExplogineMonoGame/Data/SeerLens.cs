2d3632c262095843ecc818823d7cdbed3a19e37f
notexplosive <thenotexplosive@gmail.com>
2022-11-30 17:03:35 -0800

    make SeerLens static

--- Library/ExplogineMonoGame/Data/SeerLens.cs
+++ Library/ExplogineMonoGame/Data/SeerLens.cs
@@ -7,24 +7,15 @@ namespace ExplogineMonoGame.Data;
 
 public class SeerLens
 {
-    public SeerLens(RectangleF rectangleF)
+    public static void Render(Painter painter, RectangleF viewBounds, Point outputDimensions, Action<Painter> drawContent)
     {
-        Rectangle = rectangleF;
-    }
-
-    public RectangleF Rectangle { get; set; }
-
-    public void Render(Painter painter, Canvas canvas, Action<Painter> drawContent)
-    {
-        Client.Graphics.PushCanvas(canvas);
         var translation = 
-            Matrix.Invert(Matrix.CreateTranslation(new Vector3(Rectangle.Location, 0)));
+            Matrix.Invert(Matrix.CreateTranslation(new Vector3(viewBounds.Location, 0)));
         var scale = 
-            Matrix.CreateScale(new Vector3(canvas.Size.X / Rectangle.Width, canvas.Size.Y / Rectangle.Height, 1));
+            Matrix.CreateScale(new Vector3(outputDimensions.X / viewBounds.Width, outputDimensions.Y / viewBounds.Height, 1));
         painter.BeginSpriteBatch(SamplerState.LinearWrap, translation * scale);
         painter.Clear(Color.LightBlue);
         drawContent(painter);
         painter.EndSpriteBatch();
-        Client.Graphics.PopCanvas();
     }
 }
