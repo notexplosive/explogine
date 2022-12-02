0ec3caf2ff7fd7db5a8fc11c405e7c839110e54f
notexplosive <thenotexplosive@gmail.com>
2022-12-02 09:13:20 -0800

    Transfer ZoomedOutBounds and ZoomedInBounds into RectangleF

--- Library/ExplogineMonoGame/Data/RectangleF.cs
+++ Library/ExplogineMonoGame/Data/RectangleF.cs
@@ -448,4 +448,51 @@ public struct RectangleF : IEquatable<RectangleF>
     {
         return Matrix.Invert(CanvasToScreenScalar(outputDimensions));
     }
+    
+    /// <summary>
+    ///     Deflates the ViewRect centered on a focus point such that the focus point is at the same relative position before
+    ///     and after the deflation.
+    /// </summary>
+    /// <param name="zoomAmount">
+    ///     Amount to deflate the long side of the viewBounds by (short side will deflate by the correct
+    ///     amount relative to aspect ratio)
+    /// </param>
+    /// <param name="focusPosition">Position to zoom towards in WorldSpace (aka: the same space as the ViewBounds rect)</param>
+    /// <returns></returns>
+    [Pure]
+    public RectangleF GetZoomedInBounds(float zoomAmount, Vector2 focusPosition)
+    {
+        var focusRelativeToViewBounds = focusPosition - Location;
+        var relativeScalar = focusRelativeToViewBounds.StraightDivide(Width, Height);
+        var zoomedInBounds = InflatedMaintainAspectRatio(-zoomAmount);
+
+        // center zoomed in bounds on focus
+        zoomedInBounds.Location = focusPosition - zoomedInBounds.Size / 2f;
+
+        // offset zoomed in bounds so focus is in the same relative spot
+        var focusRelativeToZoomedInBounds =
+            relativeScalar.StraightMultiply(zoomedInBounds.Width, zoomedInBounds.Height);
+        var newFocusPosition = focusRelativeToZoomedInBounds + zoomedInBounds.Location;
+        var oldFocusPosition = focusRelativeToViewBounds + Location;
+        zoomedInBounds.Offset(oldFocusPosition - newFocusPosition);
+
+        return zoomedInBounds;
+    }
+
+    /// <summary>
+    ///     Inflates the ViewRect centered on a focus point such that the focus point is at the same relative position before
+    ///     and after the deflation.
+    /// </summary>
+    /// <param name="zoomAmount">Amount to deflate the viewBounds by</param>
+    /// <param name="focusPosition">Position to zoom towards in WorldSpace (aka: the same space as the ViewBounds rect)</param>
+    /// <returns></returns>
+    [Pure]
+    public RectangleF GetZoomedOutBounds(float zoomAmount, Vector2 focusPosition)
+    {
+        var zoomedInBounds = GetZoomedInBounds(zoomAmount, focusPosition);
+        var zoomedOutOffset = Center - zoomedInBounds.Center;
+        var zoomedOutBounds = zoomedInBounds.InflatedMaintainAspectRatio(zoomAmount * 2);
+        zoomedOutBounds.Offset(zoomedOutOffset * 2);
+        return zoomedOutBounds;
+    }
 }
