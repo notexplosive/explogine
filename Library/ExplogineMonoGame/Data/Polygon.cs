222d9ed4acb5302ad04e10340ef1d90729eea073
notexplosive <thenotexplosive@gmail.com>
2022-12-01 20:47:45 -0800

    Rotate Polygon

--- Library/ExplogineMonoGame/Data/Polygon.cs
+++ Library/ExplogineMonoGame/Data/Polygon.cs
@@ -5,15 +5,50 @@ namespace ExplogineMonoGame.Data;
 
 public struct Polygon
 {
+    private float _angle;
+
     public Polygon(Vector2 centerLocation, Vector2[] relativePoints)
     {
+        _angle = 0;
         RelativePoints = relativePoints;
         CenterLocation = centerLocation;
         VertexCount = relativePoints.Length;
+        OriginalRelativePoints = new Vector2[relativePoints.Length];
+
+        for (var i = 0; i < relativePoints.Length; i++)
+        {
+            OriginalRelativePoints[i] = relativePoints[i];
+        }
+    }
+
+    public Vector2[] OriginalRelativePoints { get; }
+
+    public float Angle
+    {
+        get => _angle;
+        set
+        {
+            for (var i = 0; i < RelativePoints.Length; i++)
+            {
+                RelativePoints[i] = OriginalRelativePoints[i].Rotated(value, Vector2.Zero);
+            }
+
+            _angle = value;
+        }
     }
 
     public Vector2[] RelativePoints { get; }
     public Vector2 CenterLocation { get; }
     public int VertexCount { get; }
-    public Vector2 this[Index i] => RelativePoints[i] + CenterLocation;
+    public Vector2 this[Index i] => CenterLocation + RelativePoints[i];
+
+    /// <summary>
+    /// Rotate the Polygon clockwise around the center
+    /// </summary>
+    /// <param name="radians"></param>
+    public void Rotate(float radians)
+    {
+        Angle += radians;
+    }
+    
 }
--- Library/ExplogineMonoGame/Data/Vector2Extensions.cs
+++ Library/ExplogineMonoGame/Data/Vector2Extensions.cs
@@ -40,4 +40,16 @@ public static class Vector2Extensions
     {
         return vec.StraightMultiply(other.ToVector2());
     }
+
+    /// <summary>
+    /// Rotates the vector clockwise around an origin point
+    /// </summary>
+    /// <param name="vec"></param>
+    /// <param name="radians"></param>
+    /// <param name="origin"></param>
+    /// <returns></returns>
+    public static Vector2 Rotated(this Vector2 vec, float radians, Vector2 origin)
+    {
+        return Vector2.Transform(vec, Matrix.CreateTranslation(new Vector3(-origin, 0)) * Matrix.CreateRotationZ(radians) * Matrix.CreateTranslation(new Vector3(origin, 0)));
+    }
 }
