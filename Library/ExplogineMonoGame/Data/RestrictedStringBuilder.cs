99ebaad9eb10972e435dfc35e80019e9941f9926
notexplosive <thenotexplosive@gmail.com>
2022-12-07 22:55:41 -0800

    maybe fix a bug that doesn't exist yet

--- Library/ExplogineMonoGame/Data/RestrictedStringBuilder.cs
+++ Library/ExplogineMonoGame/Data/RestrictedStringBuilder.cs
@@ -55,6 +55,7 @@ public static class RestrictedStringBuilder
             {
                 lettersAsFragments[i] = glyphData;
                 currentFragmentIndex++;
+                charIndexWithinCurrentFragment = 0;
             }
             else
             {
