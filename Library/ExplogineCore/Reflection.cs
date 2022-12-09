44a605a43a4637ee0b6e075d1363e586f7e7d2e9
notexplosive <thenotexplosive@gmail.com>
2022-12-08 22:29:12 -0800

    Add some helpful reflection code

---
+++ Library/ExplogineCore/Reflection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace ExplogineCore;
+
+public static class Reflection
+{
+    /// <summary>
+    /// Gets static fields from type T that derive from TInterface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TInterface"></typeparam>
+    /// <returns></returns>
+    public static Dictionary<string, TInterface> GetStaticFieldsThatDeriveFromType<T, TInterface>()
+    {
+        return typeof(T)
+            .GetFields(BindingFlags.Public | BindingFlags.Static)
+            .Where(fieldInfo => fieldInfo.FieldType.GetInterfaces().Contains(typeof(TInterface)))
+            .ToDictionary(
+                fieldInfo => fieldInfo.Name,
+                fieldInfo => (TInterface) fieldInfo.GetValue(null)!
+            );
+    }
+}
