f3e8bd2b6c438ea08a55ed574b78502fb4155b02
notexplosive <thenotexplosive@gmail.com>
2022-12-15 21:50:02 -0800

    Extract file

--- Library/ExplogineMonoGame/Layout/Element.cs
+++ Library/ExplogineMonoGame/Layout/Element.cs
@@ -4,58 +4,6 @@ using Microsoft.Xna.Framework;
 
 namespace ExplogineMonoGame.Layout;
 
-public static class L
-{
-    public static Element Fixed(string name, float x, float y)
-    {
-        return new Element(new ElementName(name), new FixedEdgeSize(x), new FixedEdgeSize(y));
-    }
-
-    public static Element StretchedHorizontal(string name, float verticalSize)
-    {
-        return new Element(new ElementName(name), new StretchedEdgeSize(), new FixedEdgeSize(verticalSize));
-    }
-
-    public static Element StretchedVertical(string name, float horizontalSize)
-    {
-        return new Element(new ElementName(name), new FixedEdgeSize(horizontalSize), new StretchedEdgeSize());
-    }
-
-    public static Element FixedSpacer(float size)
-    {
-        return new Element(new ElementBlankName(), new FixedEdgeSize(size), new FixedEdgeSize(size));
-    }
-
-    public static Element StretchedBoth(string name)
-    {
-        return new Element(new ElementName(name), new StretchedEdgeSize(), new StretchedEdgeSize());
-    }
-
-    public static IElement StretchedAlong(string name, float perpendicularSize)
-    {
-        return new DynamicElement(alongAxis =>
-        {
-            return alongAxis.ReturnIfXElseY(
-                () => new Element(new ElementName(name), new StretchedEdgeSize(),
-                    new FixedEdgeSize(perpendicularSize)),
-                () => new Element(new ElementName(name), new FixedEdgeSize(perpendicularSize),
-                    new StretchedEdgeSize())
-            );
-        });
-    }
-
-    public static IElement StretchedPerpendicular(string name, float alongSize)
-    {
-        return new DynamicElement(alongAxis =>
-        {
-            return alongAxis.ReturnIfXElseY(
-                () => new Element(new ElementName(name), new FixedEdgeSize(alongSize), new StretchedEdgeSize()),
-                () => new Element(new ElementName(name), new StretchedEdgeSize(), new FixedEdgeSize(alongSize))
-            );
-        });
-    }
-}
-
 public readonly record struct Element
     (IElementName Name, IEdgeSize X, IEdgeSize Y, ElementChildren? Children = null) : IElement
 {
