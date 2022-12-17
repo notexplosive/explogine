39eeecf529beae12bc4a280e595d8a806ffa9bfb
notexplosive <thenotexplosive@gmail.com>
2022-12-16 16:35:29 -0800

    Internal cleanup in serialization code

--- Library/ExplogineMonoGame/Layout/SerializedLayoutArrangement.cs
+++ Library/ExplogineMonoGame/Layout/SerializedLayoutArrangement.cs
@@ -6,49 +6,10 @@ public static class LayoutSerialization
 {
     public static string ToJson(LayoutArrangement arrangement)
     {
-        return JsonConvert.SerializeObject(LayoutSerialization.SerializeGroup(arrangement.RawGroup), Formatting.Indented);
+        return JsonConvert.SerializeObject(new SerializedGroup(arrangement.RawGroup), Formatting.Indented);
     }
 
-    public static SerializedSettings SerializeSettings(ArrangementSettings settings)
-    {
-        var result = new SerializedSettings
-        {
-            Alignment = settings.Alignment.ToString(),
-            Margin = new[] {settings.Margin.X, settings.Margin.Y},
-            Orientation = (int) settings.Orientation
-        };
-        return result;
-    }
-
-    public static SerializedGroup SerializeGroup(LayoutElementGroup layoutLayoutElements)
-    {
-        var elements = new SerializedElement[layoutLayoutElements.Elements.Length];
-
-        for (var i = 0; i < layoutLayoutElements.Elements.Length; i++)
-        {
-            elements[i] = LayoutSerialization.SerializeElement(layoutLayoutElements.Elements[i]);
-        }
-
-        return new SerializedGroup
-        {
-            Elements = elements,
-            Settings = LayoutSerialization.SerializeSettings(layoutLayoutElements.ArrangementSettings)
-        };
-    }
-
-    public static SerializedElement SerializeElement(LayoutElement layoutElement)
-    {
-        return new SerializedElement
-        {
-            SubGroup = layoutElement.Children.HasValue
-                ? LayoutSerialization.SerializeGroup(layoutElement.Children.Value)
-                : null,
-            Size = LayoutSerialization.SerializeSize(layoutElement.X, layoutElement.Y),
-            Name = layoutElement.Name is ElementName name ? name.Text : null
-        };
-    }
-
-    public static string[] SerializeSize(IEdgeSize x, IEdgeSize y)
+    private static string[] SerializeSize(IEdgeSize x, IEdgeSize y)
     {
         return new[]
         {
@@ -62,6 +23,13 @@ public static class LayoutSerialization
         public int Orientation;
         public float[] Margin;
         public string Alignment;
+        
+        public SerializedSettings(ArrangementSettings settings)
+        {
+            Alignment = settings.Alignment.ToString();
+            Margin = new[] {settings.Margin.X, settings.Margin.Y};
+            Orientation = (int) settings.Orientation;
+        }
     }
 
     public struct SerializedElement
@@ -69,11 +37,33 @@ public static class LayoutSerialization
         public string? Name;
         public string[] Size;
         public SerializedGroup? SubGroup;
+        
+        public SerializedElement(LayoutElement layoutElement)
+        {
+            SubGroup = layoutElement.Children.HasValue
+                ? new SerializedGroup(layoutElement.Children.Value)
+                : null;
+            Size = SerializeSize(layoutElement.X, layoutElement.Y);
+            Name = layoutElement.Name is ElementName name ? name.Text : null;
+        }
     }
 
     public struct SerializedGroup
     {
         public SerializedSettings Settings;
         public SerializedElement[] Elements;
+        
+        public SerializedGroup(LayoutElementGroup layoutLayoutElements)
+        {
+            var elements = new SerializedElement[layoutLayoutElements.Elements.Length];
+
+            for (var i = 0; i < layoutLayoutElements.Elements.Length; i++)
+            {
+                elements[i] = new SerializedElement(layoutLayoutElements.Elements[i]);
+            }
+
+            Elements = elements;
+            Settings = new SerializedSettings(layoutLayoutElements.ArrangementSettings);
+        }
     }
 }
