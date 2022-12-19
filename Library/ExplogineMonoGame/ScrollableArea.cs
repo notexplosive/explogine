de4b239d05d01ccc1a9992a6468eb2cd9255913b
notexplosive <thenotexplosive@gmail.com>
2022-12-18 22:41:08 -0800

    Scrollable Area

---
+++ Library/ExplogineMonoGame/ScrollableArea.cs
@@ -0,0 +1,31 @@
+﻿using ExplogineMonoGame.Data;
+using Microsoft.Xna.Framework;
+
+namespace ExplogineMonoGame;
+
+public class ScrollableArea
+{
+    private Vector2 _viewPosition;
+
+    public ScrollableArea(Point canvasSize, RectangleF innerWorldBoundaries)
+    {
+        CanvasSize = canvasSize;
+        InnerWorldBoundaries = innerWorldBoundaries;
+    }
+
+    public Point CanvasSize { get; set; }
+    public RectangleF InnerWorldBoundaries { get; set; }
+    public RectangleF ViewBounds => new(_viewPosition, CanvasSize.ToVector2());
+    public Matrix CanvasToScreen => ViewBounds.CanvasToScreen(CanvasSize);
+    public Matrix ScreenToCanvas => ViewBounds.ScreenToCanvas(CanvasSize);
+
+    public void Move(Vector2 offset)
+    {
+        _viewPosition = ViewBounds.Moved(offset).ConstrainedTo(InnerWorldBoundaries).TopLeft;
+    }
+
+    public void SetPosition(Vector2 position)
+    {
+        _viewPosition = (ViewBounds with {Location = Vector2.Zero}).ConstrainedTo(InnerWorldBoundaries).TopLeft;
+    }
+}
