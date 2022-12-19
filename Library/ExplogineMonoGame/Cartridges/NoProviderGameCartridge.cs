8a39957937d92b0d211a555e4866795408b745bd
notexplosive <thenotexplosive@gmail.com>
2022-12-18 22:40:00 -0800

    NoProviderGameCartridge

---
+++ Library/ExplogineMonoGame/Cartridges/NoProviderGameCartridge.cs
@@ -0,0 +1,22 @@
+﻿using ExplogineMonoGame.Data;
+using ExplogineMonoGame.Input;
+
+namespace ExplogineMonoGame.Cartridges;
+
+public abstract class NoProviderGameCartridge : ICartridge
+{
+    public abstract void OnCartridgeStarted();
+    public abstract void Update(float dt);
+    public abstract void Draw(Painter painter);
+    public abstract void UpdateInput(InputFrameState input, HitTestStack hitTestStack);
+    public virtual void Unload()
+    {
+    }
+
+    public bool ShouldLoadNextCartridge()
+    {
+        return false;
+    }
+
+    public abstract CartridgeConfig CartridgeConfig { get; }
+}
