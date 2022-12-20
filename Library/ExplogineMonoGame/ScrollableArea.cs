fe382c9bd40b33c5ca77e38efd15d51210335a88
notexplosive <thenotexplosive@gmail.com>
2022-12-19 17:41:44 -0800

    ScrollableArea owns scrollbars

--- Library/ExplogineMonoGame/ScrollableArea.cs
+++ Library/ExplogineMonoGame/ScrollableArea.cs
@@ -1,16 +1,24 @@
-﻿using ExplogineMonoGame.Data;
+﻿using ExplogineCore.Data;
+using ExplogineMonoGame.Data;
+using ExplogineMonoGame.Gui;
+using ExplogineMonoGame.Input;
 using Microsoft.Xna.Framework;
 
 namespace ExplogineMonoGame;
 
-public class ScrollableArea
+public class ScrollableArea : IUpdateInput
 {
     private Vector2 _viewPosition;
+    private Scrollbar _verticalScrollbar;
+    private Scrollbar _horizontalScrollbar;
 
     public ScrollableArea(Point canvasSize, RectangleF innerWorldBoundaries)
     {
         CanvasSize = canvasSize;
         InnerWorldBoundaries = innerWorldBoundaries;
+        
+        _verticalScrollbar = new Scrollbar(this, Orientation.Vertical, Depth.Middle);
+        _horizontalScrollbar = new Scrollbar(this, Orientation.Horizontal, Depth.Middle);
     }
 
     public Point CanvasSize { get; set; }
@@ -27,5 +35,17 @@ public class ScrollableArea
     public void SetPosition(Vector2 position)
     {
         _viewPosition = (ViewBounds with {Location = position}).ConstrainedTo(InnerWorldBoundaries).TopLeft;
+    }
+
+    public void DrawScrollbars(Painter painter, SimpleGuiTheme theme)
+    {
+        theme.DrawScrollbar(painter, _verticalScrollbar);
+        theme.DrawScrollbar(painter, _horizontalScrollbar);
+    }
+
+    public void UpdateInput(InputFrameState input, HitTestStack hitTestStack)
+    {
+        _verticalScrollbar.UpdateInput(input, hitTestStack);
+        _horizontalScrollbar.UpdateInput(input, hitTestStack);
     }
 }
