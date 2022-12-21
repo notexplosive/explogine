006d2ec7e98f9e475324ad0c314e0825b70b4e81
notexplosive <thenotexplosive@gmail.com>
2022-12-20 17:13:07 -0800

    Create sub object

--- Library/ExplogineMonoGame/Data/TextInputWidget.cs
+++ Library/ExplogineMonoGame/Data/TextInputWidget.cs
@@ -12,17 +12,19 @@ namespace ExplogineMonoGame.Data;
 public class TextInputWidget : Widget, IUpdateInput
 {
     private readonly Alignment _alignment = Alignment.TopLeft;
-    private readonly List<char> _content = new();
     private readonly IFontGetter _font;
     private int _cursorIndex;
     private int? _hoveredLetterIndex;
     private LeftRight _hoveredSide;
     private bool _selected;
+    private readonly CharSequence _charSequence;
 
     public TextInputWidget(Vector2 position, Point size, IFontGetter font, Depth depth) : base(position, size, depth)
     {
         _font = font;
-        _content.AddRange(new[] {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'});
+        
+        // Content will need to be rebuilt every time these values change
+        _charSequence = new CharSequence(font, InnerRectangle, _alignment);
     }
 
     public RectangleF InnerRectangle => new RectangleF(Vector2.Zero, Rectangle.Size).Inflated(-5, -5);
@@ -56,19 +58,19 @@ public class TextInputWidget : Widget, IUpdateInput
                 {
                     var offset = _hoveredSide == LeftRight.Right ? 1 : 0;
                     _cursorIndex = _hoveredLetterIndex.Value + offset;
-                    if (_cursorIndex > _content.Count)
+                    if (_cursorIndex > _charSequence.NumberOfChars)
                     {
-                        _cursorIndex = _content.Count;
+                        _cursorIndex = _charSequence.NumberOfChars;
                     }
                 }
             }
 
             innerHitTestStack.BeforeLayerResolved += () => { _hoveredLetterIndex = null; };
 
-            for (var i = 0; i < _content.Count; i++)
+            for (var i = 0; i < _charSequence.NumberOfChars; i++)
             {
                 var index = i; // index will be captured so we need to set aside a variable
-                var charRectangle = RectangleAtCharIndex(i);
+                var charRectangle = _charSequence.RectangleAtCharIndex(i);
                 var halfWidth = charRectangle.Size.X / 2;
                 var leftRectangle = new RectangleF(charRectangle.Location,
                     new Vector2(halfWidth, charRectangle.Size.Y));
@@ -96,9 +98,58 @@ public class TextInputWidget : Widget, IUpdateInput
         }
     }
 
+    public void PrepareDraw(Painter painter)
+    {
+        Client.Graphics.PushCanvas(Canvas);
+        painter.BeginSpriteBatch();
+        var depth = Depth.Middle;
+
+        painter.Clear(_selected ? Color.LightBlue : Color.White);
+
+        if (_selected)
+        {
+            var cursorRect = _charSequence.RectangleAtCharIndex(_cursorIndex);
+            var size = cursorRect.Size;
+            size.X = 2f;
+            cursorRect.Offset(-size.X / 2f, 0);
+            var cursorRectangle = new RectangleF(cursorRect.TopLeft, size);
+            painter.DrawRectangle(cursorRectangle, new DrawSettings {Depth = depth - 1, Color = Color.Black});
+        }
+
+        painter.DrawStringWithinRectangle(_font, _charSequence.ContentString(), InnerRectangle, _alignment,
+            new DrawSettings {Color = Color.Black, Depth = depth});
+
+        if (_hoveredLetterIndex.HasValue)
+        {
+            var charRectangle = _charSequence.RectangleAtCharIndex(_hoveredLetterIndex.Value);
+            var halfWidth = charRectangle.Size.X / 2;
+            var leftRectangle = new RectangleF(charRectangle.Location,
+                new Vector2(halfWidth, charRectangle.Size.Y));
+
+            var rightRectangle = new RectangleF(charRectangle.Location,
+                    new Vector2(halfWidth, charRectangle.Size.Y))
+                .Moved(new Vector2(halfWidth, 0));
+
+            if (Client.Debug.IsActive)
+            {
+                painter.DrawRectangle(_hoveredSide == LeftRight.Left ? leftRectangle : rightRectangle,
+                    new DrawSettings
+                    {
+                        Depth = depth + 1,
+                        Color = _hoveredSide == LeftRight.Left
+                            ? Color.Red.WithMultipliedOpacity(0.2f)
+                            : Color.Blue.WithMultipliedOpacity(0.2f)
+                    });
+            }
+        }
+
+        painter.EndSpriteBatch();
+        Client.Graphics.PopCanvas();
+    }
+
     private void MoveRight()
     {
-        if (_cursorIndex < _content.Count)
+        if (_cursorIndex < _charSequence.NumberOfChars)
         {
             _cursorIndex++;
         }
@@ -114,10 +165,7 @@ public class TextInputWidget : Widget, IUpdateInput
 
     private void ReverseBackspace()
     {
-        if (_cursorIndex < _content.Count)
-        {
-            _content.RemoveAt(_cursorIndex);
-        }
+        _charSequence.RemoveAt(_cursorIndex);
     }
 
     private void EnterText(char[] enteredCharacters)
@@ -152,7 +200,7 @@ public class TextInputWidget : Widget, IUpdateInput
 
     private void EnterCharacter(char character)
     {
-        _content.Insert(_cursorIndex, character);
+        _charSequence.Insert(_cursorIndex, character);
         _cursorIndex++;
     }
 
@@ -161,107 +209,97 @@ public class TextInputWidget : Widget, IUpdateInput
         if (_cursorIndex > 0)
         {
             _cursorIndex--;
-            _content.RemoveAt(_cursorIndex);
+            _charSequence.RemoveAt(_cursorIndex);
         }
     }
 
-    public void PrepareDraw(Painter painter)
+    private class CharSequence
     {
-        Client.Graphics.PushCanvas(Canvas);
-        painter.BeginSpriteBatch();
-        var depth = Depth.Middle;
+        private readonly IFontGetter _font;
+        private readonly RectangleF _innerRectangle;
+        private readonly Alignment _alignment;
+        private readonly List<char> _content = new();
 
-        painter.Clear(_selected ? Color.LightBlue : Color.White);
-
-        if (_selected)
+        public CharSequence(IFontGetter font, RectangleF innerRectangle, Alignment alignment)
         {
-            var cursorRect = RectangleAtCharIndex(_cursorIndex);
-            var size = cursorRect.Size;
-            size.X = 2f;
-            cursorRect.Offset(-size.X / 2f, 0);
-            var cursorRectangle = new RectangleF(cursorRect.TopLeft, size);
-            painter.DrawRectangle(cursorRectangle, new DrawSettings {Depth = depth - 1, Color = Color.Black});
+            _font = font;
+            _innerRectangle = innerRectangle;
+            _alignment = alignment;
+            _content.AddRange(new[] {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', char.MinValue});
         }
 
-        painter.DrawStringWithinRectangle(_font, ContentString(), InnerRectangle, _alignment,
-            new DrawSettings {Color = Color.Black, Depth = depth});
+        public int NumberOfNodes => _content.Count;
+        public int NumberOfChars => _content.Count - 1;
 
-        if (_hoveredLetterIndex.HasValue)
+        [Pure]
+        public string ContentString()
         {
-            var charRectangle = RectangleAtCharIndex(_hoveredLetterIndex.Value);
-            var halfWidth = charRectangle.Size.X / 2;
-            var leftRectangle = new RectangleF(charRectangle.Location,
-                new Vector2(halfWidth, charRectangle.Size.Y));
-
-            var rightRectangle = new RectangleF(charRectangle.Location,
-                    new Vector2(halfWidth, charRectangle.Size.Y))
-                .Moved(new Vector2(halfWidth, 0));
-
-            if (Client.Debug.IsActive)
+            // todo: cache this!!
+            var stringBuilder = new StringBuilder();
+            foreach (var item in _content)
             {
-                painter.DrawRectangle(_hoveredSide == LeftRight.Left ? leftRectangle : rightRectangle,
-                    new DrawSettings
-                    {
-                        Depth = depth + 1,
-                        Color = _hoveredSide == LeftRight.Left
-                            ? Color.Red.WithMultipliedOpacity(0.2f)
-                            : Color.Blue.WithMultipliedOpacity(0.2f)
-                    });
+                if (item != '\0')
+                {
+                    stringBuilder.Append(item);
+                }
             }
+
+            return stringBuilder.ToString();
         }
 
-        painter.EndSpriteBatch();
-        Client.Graphics.PopCanvas();
-    }
-
-    [Pure]
-    private string ContentString()
-    {
-        // todo: cache this!!
-        var stringBuilder = new StringBuilder();
-        foreach (var item in _content)
+        [Pure]
+        public char CharacterAt(int index)
         {
-            stringBuilder.Append(item);
+            return ContentString()[index];
         }
 
-        return stringBuilder.ToString();
-    }
-
-    [Pure]
-    private char CharacterAt(int index)
-    {
-        return ContentString()[index];
-    }
-
-    [Pure]
-    private RectangleF RectangleAtCharIndex(int targetIndex)
-    {
-        // todo: cache this!!
-        var contentString = ContentString();
-        // If the current buffer is empty, we act like we have just one character so we format it in the right spot
-        var formattedText = new FormattedText(_font, contentString.Length > 0 ? contentString : " ");
-
-        var currentRect = new RectangleF(Vector2.Zero, new Vector2(0, _font.GetFont().Height));
-
-        var glyphIndex = 0;
-        foreach (var glyph in formattedText.GetGlyphs(InnerRectangle, _alignment))
+        [Pure]
+        public RectangleF RectangleAtCharIndex(int targetIndex)
         {
-            currentRect = new RectangleF(glyph.Position, glyph.Data.Size);
+            // todo: cache this!!
+            var contentString = ContentString();
+            // If the current buffer is empty, we act like we have just one character so we format it in the right spot
+            var formattedText = new FormattedText(_font, contentString.Length > 0 ? contentString : " ");
+
+            var currentRect = new RectangleF(Vector2.Zero, new Vector2(0, _font.GetFont().Height));
+
+            var glyphIndex = 0;
+            foreach (var glyph in formattedText.GetGlyphs(_innerRectangle, _alignment))
+            {
+                currentRect = new RectangleF(glyph.Position, glyph.Data.Size);
+                if (glyphIndex == targetIndex)
+                {
+                    return currentRect;
+                }
+
+                glyphIndex++;
+            }
+
             if (glyphIndex == targetIndex)
             {
-                return currentRect;
+                // We're at the very end of the string, we want a zero-width rect at the end of the string 
+                return new RectangleF(new Vector2(currentRect.X + currentRect.Width, currentRect.Y),
+                    new Vector2(0, _font.GetFont().Height));
             }
 
-            glyphIndex++;
+            throw new IndexOutOfRangeException();
         }
 
-        if (glyphIndex == targetIndex)
+        public void RemoveAt(int cursorIndex)
         {
-            // We're at the very end of the string, we want a zero-width rect at the end of the string 
-            return new RectangleF(new Vector2(currentRect.X + currentRect.Width, currentRect.Y),
-                new Vector2(0, _font.GetFont().Height));
+            if (cursorIndex != NumberOfNodes - 1)
+            {
+                _content.RemoveAt(cursorIndex);
+            }
+            else
+            {
+                Client.Debug.LogWarning("Attempted to remove null terminator");
+            }
         }
 
-        throw new IndexOutOfRangeException();
+        public void Insert(int cursorIndex, char character)
+        {
+            _content.Insert(cursorIndex, character);
+        }
     }
 }
