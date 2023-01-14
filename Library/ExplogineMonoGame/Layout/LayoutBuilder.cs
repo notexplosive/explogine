b3d14cd4eff52d14e0548fa00f1fdd39602d49f5
notexplosive <thenotexplosive@gmail.com>
2023-01-14 01:52:15 -0800

    Add LayoutBuilder

---
+++ Library/ExplogineMonoGame/Layout/LayoutBuilder.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace ExplogineMonoGame.Layout;
+
+public class LayoutBuilder
+{
+    private readonly List<Node> _nodes = new();
+    private readonly Style _style;
+
+    public LayoutBuilder(Style style)
+    {
+        _style = style;
+    }
+
+    public void Add(LayoutElement element)
+    {
+        if (element.Children.HasValue)
+        {
+            AddGroup(element.Children.Value.Style, element);
+        }
+        else
+        {
+            _nodes.Add(new Node(element.Name, element.X, element.Y, null));
+        }
+    }
+
+    public LayoutBuilder AddGroup(Style style, LayoutElement parentElement)
+    {
+        var group = new LayoutBuilder(style);
+
+        if (parentElement.Children.HasValue)
+        {
+            foreach (var child in parentElement.Children.Value.Elements)
+            {
+                group.Add(child);
+            }
+        }
+
+        _nodes.Add(new Node(parentElement.Name, parentElement.X, parentElement.Y, group));
+
+        return group;
+    }
+
+    public LayoutElementGroup ToLayoutGroup()
+    {
+        var elements = new List<LayoutElement>();
+
+        foreach (var node in _nodes)
+        {
+            elements.Add(node.ToElement());
+        }
+
+        return L.Root(_style, elements.ToArray());
+    }
+
+    private class Node
+    {
+        private readonly LayoutBuilder? _children;
+        private readonly IElementName _name;
+        private readonly IEdgeSize _x;
+        private readonly IEdgeSize _y;
+
+        public Node(IElementName name, IEdgeSize x, IEdgeSize y, LayoutBuilder? children)
+        {
+            _name = name;
+            _x = x;
+            _y = y;
+            _children = children;
+        }
+
+        public LayoutElement ToElement()
+        {
+            return new LayoutElement(_name, _x, _y, _children?.ToLayoutGroup());
+        }
+    }
+}
