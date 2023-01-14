20136fc18c683afb742db54ae6d32f6c56a05145
notexplosive <thenotexplosive@gmail.com>
2023-01-14 01:52:10 -0800

    Add CartridgePlayer

---
+++ Library/ExplogineMonoGame/Data/CartridgePlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ExplogineMonoGame.Cartridges;
+using ExplogineMonoGame.Rails;
+
+namespace ExplogineMonoGame.Data;
+
+public interface ICartridgePlayer : IUpdateHook, IUpdateInputHook, IDrawHook
+{
+}
+
+public class CartridgePlayer<TCartridge> : ICartridgePlayer where TCartridge : Cartridge
+{
+    private readonly TCartridge _cartridge;
+    private readonly IWindow _window;
+
+    public CartridgePlayer(IWindow window)
+    {
+        _window = window;
+        var constructedCartridge =
+            (TCartridge?) Activator.CreateInstance(typeof(TCartridge), new App(window, new ClientFileSystem()));
+
+        _cartridge = constructedCartridge ??
+                     throw new Exception($"Activator could not create instance of {typeof(TCartridge).Name}");
+
+        // assumes load events were already called
+        _cartridge.OnCartridgeStarted();
+    }
+
+    public void Update(float dt)
+    {
+        _cartridge.Update(dt);
+    }
+
+    public void Draw(Painter painter)
+    {
+        _cartridge.Draw(painter);
+    }
+
+    public void UpdateInput(ConsumableInput input, HitTestStack hitTestStack)
+    {
+        _cartridge.UpdateInput(input, hitTestStack);
+    }
+}
