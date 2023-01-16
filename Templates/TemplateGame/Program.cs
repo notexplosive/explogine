9b48bf6b1c576a47d91c0fc7981f47d952f73120
notexplosive <thenotexplosive@gmail.com>
2023-01-15 18:38:51 -0800

    fix template

--- Templates/TemplateGame/Program.cs
+++ Templates/TemplateGame/Program.cs
@@ -8,4 +8,4 @@ var config = new WindowConfigWritable
     WindowSize = new Point(1600, 900),
     Title = "NotExplosive.net"
 };
-Bootstrap.Run(args, new WindowConfig(config), new BlankCartridge());
+Bootstrap.Run(args, new WindowConfig(config), (runtime) => new BlankCartridge(runtime));
