8a8af965da011c22775b426375c7e4bf7d779c3e
notexplosive <thenotexplosive@gmail.com>
2023-01-20 21:11:24 -0800

    BakedLayoutElement implicitly casts to rectangle

--- Library/ExplogineMonoGame/Layout/BakedLayoutElement.cs
+++ Library/ExplogineMonoGame/Layout/BakedLayoutElement.cs
@@ -2,4 +2,10 @@
 
 namespace ExplogineMonoGame.Layout;
 
-public readonly record struct BakedLayoutElement(RectangleF Rectangle, string Name, int NestingLevel);
+public readonly record struct BakedLayoutElement(RectangleF Rectangle, string Name, int NestingLevel)
+{
+    public static implicit operator RectangleF(BakedLayoutElement layoutElement)
+    {
+        return layoutElement.Rectangle;
+    }
+}
