79e6a63955468740f7e70b0b086eb3040276b508
notexplosive <thenotexplosive@gmail.com>
2023-02-01 08:21:25 -0800

    ImageAsset can draw in an explogine way

--- Library/ExplogineMonoGame/Data/ImageAsset.cs
+++ Library/ExplogineMonoGame/Data/ImageAsset.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ExplogineMonoGame.AssetManagement;
+﻿using ExplogineMonoGame.AssetManagement;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -8,10 +7,10 @@ namespace ExplogineMonoGame.Data;
 public class ImageAsset : Asset
 {
     private readonly IndirectTexture _indirectTexture;
-    public Texture2D Texture => _indirectTexture.Get();
-    public Rectangle SourceRectangle { get; }
 
-    public ImageAsset(Texture2D texture, Rectangle sourceRectangle, bool ownsTexture = false) : base(ownsTexture ? texture : null)
+    public ImageAsset(Texture2D texture, Rectangle sourceRectangle, bool ownsTexture = false) : base(ownsTexture
+        ? texture
+        : null)
     {
         _indirectTexture = new IndirectTexture(texture);
         SourceRectangle = sourceRectangle;
@@ -21,5 +20,20 @@ public class ImageAsset : Asset
     {
         _indirectTexture = indirectTexture;
         SourceRectangle = sourceRectangle;
+    }
+
+    public Texture2D Texture => _indirectTexture.Get();
+    public Rectangle SourceRectangle { get; }
+
+    public void DrawAtPosition(Painter painter, Vector2 position, Scale2D scale, DrawSettings settings)
+    {
+        settings.SourceRectangle ??= SourceRectangle;
+        painter.DrawAtPosition(Texture, position, scale, settings);
+    }
+
+    public void DrawAsRectangle(Painter painter, RectangleF rectangle, DrawSettings settings)
+    {
+        settings.SourceRectangle ??= SourceRectangle;
+        painter.DrawAsRectangle(Texture, rectangle, settings);
     }
 }
