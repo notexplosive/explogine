2ab6d339d34f6694f9793b81a1f31b9fa34dce67
notexplosive <thenotexplosive@gmail.com>
2023-02-04 13:53:13 -0800

    fix sample project

--- Examples/SampleGame/Program.cs
+++ Examples/SampleGame/Program.cs
@@ -8,4 +8,4 @@ var config = new WindowConfigWritable
     WindowSize = new Point(1600, 900),
     Title = "Example Cartridge"
 };
-Bootstrap.Run(args, new WindowConfig(config), new SampleGameCartridge());
+Bootstrap.Run(args, new WindowConfig(config), runtime => new SampleGameCartridge(runtime));
--- Examples/SampleGame/SampleGameCartridge.cs
+++ Examples/SampleGame/SampleGameCartridge.cs
@@ -18,6 +18,10 @@ public class SampleGameCartridge : BasicGameCartridge
     private HitTestTarget _c2;
     private HitTestTarget _c3;
 
+    public SampleGameCartridge(IRuntime runtime) : base(runtime)
+    {
+    }
+
     public override CartridgeConfig CartridgeConfig { get; } = new(new Point(1600 / 2, 900 / 2));
 
     public override void OnCartridgeStarted()
@@ -34,24 +38,34 @@ public class SampleGameCartridge : BasicGameCartridge
         }
     }
 
+    public record HitTestTarget(Rectangle Rectangle, Depth Depth)
+    {
+        public readonly HoverState HoverState = new HoverState();
+    }
+
     public override void Update(float dt)
     {
-        if (Client.Input.Keyboard.GetButton(Keys.F).WasPressed)
+        
+    }
+
+    public override void UpdateInput(ConsumableInput input, HitTestStack hitTestStack)
+    {
+        hitTestStack.AddZone(_c1.Rectangle, _c1.Depth, _c1.HoverState);
+        hitTestStack.AddZone(_c2.Rectangle, _c2.Depth, _c2.HoverState);
+        hitTestStack.AddZone(_c3.Rectangle, _c3.Depth, _c3.HoverState);
+        
+        if (input.Keyboard.GetButton(Keys.F).WasPressed)
         {
-            Client.Window.SetFullscreen(!Client.Window.IsFullscreen);
+            Runtime.Window.SetFullscreen(!Runtime.Window.IsFullscreen);
         }
 
-        Client.HitTesting.Add(_c1);
-        Client.HitTesting.Add(_c2);
-        Client.HitTesting.Add(_c3);
-
-        if (Client.Input.Keyboard.GetButton(Keys.Space).WasPressed)
+        if (input.Keyboard.GetButton(Keys.Space).WasPressed)
         {
             Client.Debug.Log(DateTime.UtcNow.Second);
             Client.Debug.Log("very\n very\n very\n very long message");
         }
 
-        if (Client.Input.Keyboard.GetButton(Keys.A).WasPressed)
+        if (input.Keyboard.GetButton(Keys.A).WasPressed)
         {
             throw new Exception("died during update loop");
         }
@@ -60,11 +74,11 @@ public class SampleGameCartridge : BasicGameCartridge
     public override void Draw(Painter painter)
     {
         painter.Clear(Color.CornflowerBlue);
-        painter.BeginSpriteBatch(SamplerState.PointWrap);
+        painter.BeginSpriteBatch();
 
         void DrawCollider(HitTestTarget collider, Color color)
         {
-            if (Client.HitTesting.LastTopHit == collider)
+            if (collider.HoverState)
             {
                 color = Color.Blue;
             }
@@ -77,12 +91,12 @@ public class SampleGameCartridge : BasicGameCartridge
         DrawCollider(_c2, Color.Aqua);
         DrawCollider(_c3, Color.Coral);
 
-        painter.DrawAtPosition(Client.Assets.GetTexture("noise"), Vector2.Zero);
+        painter.DrawAtPosition(Client.Assets.GetTexture("noise"), Vector2.Zero, Scale2D.One, new DrawSettings{Depth = Depth.Back});
 
-        var mousePosition = Client.Input.Mouse.Position(Client.RenderCanvas.ScreenToCanvas);
+        var mousePosition = Client.Input.Mouse.Position(Runtime.Window.ScreenToCanvas);
 
         painter.DrawStringAtPosition(Client.Assets.GetFont("engine/console-font", 20),
-            mousePosition.ToPoint().ToString(), Point.Zero, new DrawSettings());
+            mousePosition.ToPoint().ToString(), Vector2.Zero, new DrawSettings());
         painter.DrawAsRectangle(Client.Assets.GetTexture("white-pixel"),
             new Rectangle(mousePosition.ToPoint(), new Point(10, 10)), new DrawSettings());
 
@@ -94,14 +108,14 @@ public class SampleGameCartridge : BasicGameCartridge
         parameters.RegisterParameter<string>("echo");
     }
 
-    public override IEnumerable<LoadEvent?> LoadEvents(Painter painter)
+    public override IEnumerable<ILoadEvent> LoadEvents(Painter painter)
     {
-        yield return new LoadEvent("noise", () =>
+        yield return new AssetLoadEvent("noise", () =>
         {
             var canvas = new Canvas(100, 100);
             Client.Graphics.PushCanvas(canvas);
 
-            painter.BeginSpriteBatch(SamplerState.LinearWrap);
+            painter.BeginSpriteBatch();
             painter.Clear(Color.Transparent);
             var noise = new Noise(Client.Random.Seed);
 
