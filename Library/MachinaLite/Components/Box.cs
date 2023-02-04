268c4b2afb54badc80ea41d1995ec72c7e6699f9
notexplosive <thenotexplosive@gmail.com>
2023-02-04 14:05:26 -0800

    Import SpriteRenderer into MachinaLite

--- Library/MachinaLite/Components/Box.cs
+++ Library/MachinaLite/Components/Box.cs
@@ -15,4 +15,9 @@ public class Box : BaseComponent
     public Rectangle Rectangle => new(Transform.Position.ToPoint() - Offset, Size);
     public Rectangle RectangleWithoutOffset => new(Transform.Position.ToPoint(), Size);
     public Rectangle RectangleAtOrigin => new(Point.Zero, Size);
+
+    public void SetOffsetToCenter()
+    {
+        Offset = (Size.ToVector2() / 2).ToPoint();
+    }
 }
