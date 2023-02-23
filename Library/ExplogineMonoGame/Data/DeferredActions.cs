b4f1f054005ab753f8d033a6b37ebe6fa38d2d1c
notexplosive <thenotexplosive@gmail.com>
2023-02-22 23:06:27 -0800

    deferred actions execute instantly if we're in the middle of executing

--- Library/ExplogineMonoGame/Data/DeferredActions.cs
+++ Library/ExplogineMonoGame/Data/DeferredActions.cs
@@ -6,19 +6,28 @@ namespace ExplogineMonoGame.Data;
 public class DeferredActions
 {
     private readonly List<Action> _list = new();
+    private bool _isRunning;
 
     public void Add(Action action)
     {
-        _list.Add(action);
+        if (_isRunning)
+        {
+            action();
+        }
+        else
+        {
+            _list.Add(action);
+        }
     }
 
     public void RunAllAndClear()
     {
+        _isRunning = true;
         foreach (var item in _list)
         {
             item.Invoke();
         }
-        
+        _isRunning = false;
         _list.Clear();
     }
 }
