bd8c932d526016af72aa2720d6c1f6ab84b22ce0
notexplosive <thenotexplosive@gmail.com>
2023-04-16 13:04:00 -0700

    update draw as rectangle to support subpixels

--- Library/ExplogineMonoGame/Painter.cs
+++ Library/ExplogineMonoGame/Painter.cs
@@ -229,14 +229,22 @@ public class Painter
             });
         }
         settings.SourceRectangle ??= texture.Bounds;
-
+        
         // the origin is relative to the source rect, but we pass it in assume its scaled with the destination rect
         var origin = settings.Origin.Calculate(settings.SourceRectangle.Value.Size);
 
+        var scale = new Scale2D(settings.SourceRectangle.Value.Size.ToVector2().StraightDivide(destinationRectangle.Size));
+        
+        settings = settings with {Origin = new DrawOrigin(origin)};
+
+        DrawAtPosition(texture, destinationRectangle.Location, scale, settings);
+
+        /*
         // destination is downcast to a Rectangle
         _spriteBatch.Draw(texture, destinationRectangle.ToRectangle(), settings.SourceRectangle, settings.Color,
             settings.Angle,
             origin, settings.FlipEffect, settings.Depth);
+            */
     }
 
     #endregion
