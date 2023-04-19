947d7228c7f5f63c9413b5655657cd2bd11c4145
notexplosive <thenotexplosive@gmail.com>
2023-04-18 19:35:52 -0700

    Framerate counter shows draws per second

--- Library/ExplogineMonoGame/Debugging/FramerateCounter.cs
+++ Library/ExplogineMonoGame/Debugging/FramerateCounter.cs
@@ -7,24 +7,28 @@ namespace ExplogineMonoGame.Debugging;
 
 public class FramerateCounter : IUpdateHook, IDrawHook
 {
-    private float _framerate;
-    private DateTime _previousFrame;
+    private float _updatesPerSecond;
+    private DateTime _previousUpdate;
+    private DateTime _previousDraw;
 
     public void Draw(Painter painter)
     {
-        if (_framerate > 0)
+        var timeSinceLastDraw = DateTime.Now - _previousDraw;
+        float drawsPerSecond = 1f / (float)timeSinceLastDraw.TotalSeconds;
+        _previousDraw = DateTime.Now;
+        
+        if (_updatesPerSecond > 0)
         {
             painter.BeginSpriteBatch();
-            painter.DrawDebugStringAtPosition(_framerate.ToString("F0"), Vector2.Zero, new DrawSettings());
+            painter.DrawDebugStringAtPosition($"{_updatesPerSecond:F0} UPS / {drawsPerSecond:F0} FPS" , Vector2.Zero, new DrawSettings());
             painter.EndSpriteBatch();
         }
     }
 
     public void Update(float dt)
     {
-        var timeSinceLastFrame = DateTime.Now - _previousFrame;
-        _framerate = 1f / (float)timeSinceLastFrame.TotalSeconds;
-
-        _previousFrame = DateTime.Now;
+        var timeSinceLastDraw = DateTime.Now - _previousUpdate;
+        _updatesPerSecond = 1f / (float)timeSinceLastDraw.TotalSeconds;
+        _previousUpdate = DateTime.Now;
     }
 }
