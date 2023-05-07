d8cdf74d32d622db0a79c6f0b51543f369b6a29c
notexplosive <thenotexplosive@gmail.com>
2023-05-07 13:24:22 -0700

    RemoveFileExtension StringExtension

--- Library/ExplogineCore/StringExtensions.cs
+++ Library/ExplogineCore/StringExtensions.cs
@@ -1,14 +1,25 @@
-﻿namespace ExplogineCore;
+﻿using System.Diagnostics.Contracts;
+
+namespace ExplogineCore;
 
 public static class StringExtensions
 {
+    [Pure]
     public static string[] SplitLines(this string str)
     {
         return str.Split(new[] {"\r\n", "\r", "\n"}, StringSplitOptions.None);
     }
 
+    [Pure]
     public static string[] SplitDirectorySeparators(this string str)
     {
         return str.Split(new[] {"\\", "/"}, StringSplitOptions.None);
+    }
+
+    [Pure]
+    public static string RemoveFileExtension(this string str)
+    {
+        var fileInfo = new FileInfo(str);
+        return fileInfo.Name.Substring(0, fileInfo.Name.Length - fileInfo.Extension.Length);
     }
 }
