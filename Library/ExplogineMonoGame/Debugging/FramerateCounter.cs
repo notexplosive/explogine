2f75bfb7c907a74493fa7df01602da45ba8a4361
notexplosive <thenotexplosive@gmail.com>
2024-02-16 07:29:20 -0800

    improve framerate counter

--- Library/ExplogineMonoGame/Debugging/FramerateCounter.cs
+++ Library/ExplogineMonoGame/Debugging/FramerateCounter.cs
@@ -8,37 +8,29 @@ namespace ExplogineMonoGame.Debugging;
 
 public class FramerateCounter : IUpdateHook, IDrawHook
 {
-    private float _updatesPerSecond;
-    private DateTime _previousUpdate;
     private DateTime _previousDraw;
 
     public void Draw(Painter painter)
     {
         var timeSinceLastDraw = DateTime.Now - _previousDraw;
-        float drawsPerSecond = 1f / (float)timeSinceLastDraw.TotalSeconds;
+        float drawDuration = (float)timeSinceLastDraw.TotalSeconds;
         _previousDraw = DateTime.Now;
         
-        if (_updatesPerSecond > 0)
-        {
-            var memoryUsageStat = string.Empty;
+        var memoryUsageStat = string.Empty;
 
-            if (Client.Debug.MonitorMemoryUsage)
-            {
-                var currentProc = Process.GetCurrentProcess();
-                long memoryUsed = currentProc.PrivateMemorySize64;
-                memoryUsageStat = (memoryUsed / 1000_000f).ToString("F2") + " MB";
-            }
-            
-            painter.BeginSpriteBatch();
-            painter.DrawDebugStringAtPosition($"{memoryUsageStat} {_updatesPerSecond:F0} UPS / {drawsPerSecond:F0} FPS" , Vector2.Zero, new DrawSettings());
-            painter.EndSpriteBatch();
+        if (Client.Debug.MonitorMemoryUsage)
+        {
+            var currentProc = Process.GetCurrentProcess();
+            long memoryUsed = currentProc.PrivateMemorySize64;
+            memoryUsageStat = (memoryUsed / 1000_000f).ToString("F2") + " MB ";
         }
+
+        painter.BeginSpriteBatch();
+        painter.DrawDebugStringAtPosition($"{memoryUsageStat}{drawDuration:F4}ms" , Vector2.Zero, new DrawSettings{Color = Color.White});
+        painter.EndSpriteBatch();
     }
 
     public void Update(float dt)
     {
-        var timeSinceLastDraw = DateTime.Now - _previousUpdate;
-        _updatesPerSecond = 1f / (float)timeSinceLastDraw.TotalSeconds;
-        _previousUpdate = DateTime.Now;
     }
 }
