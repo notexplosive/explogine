d34252ec51184cdd53a40405cf5be912b0109d27
notexplosive <thenotexplosive@gmail.com>
2024-03-15 21:36:27 -0700

    Support threaded loading

---
+++ Library/ExplogineMonoGame/AssetManagement/UncompressedSound.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Audio;
+
+namespace ExplogineMonoGame.AssetManagement;
+
+public class UncompressedSound
+{
+    public AudioChannels Channels { get; }
+    public float[] Frames { get; }
+    public int Length { get; }
+    public int SampleRate { get; }
+
+    public UncompressedSound(float[] frames, int length, AudioChannels channels, int sampleRate)
+    {
+        Frames = frames;
+        Length = length;
+        Channels = channels;
+        SampleRate = sampleRate;
+    }
+}
