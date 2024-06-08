e71427f29c72f5f8b4e72fff7f27a0dddb82ddd8
notexplosive <thenotexplosive@gmail.com>
2024-06-08 15:56:15 -0700

    extract SokoMaker Lua into Explogine

---
+++ Library/ExplogineCore/Lua/LuaError.cs
@@ -0,0 +1,19 @@
+﻿using MoonSharp.Interpreter;
+
+namespace ExplogineCore.Lua;
+
+#pragma warning disable CS8974
+/// <summary>
+/// Used internally to manage error propagation
+/// </summary>
+[LuaBoundType]
+public class LuaError
+{
+    [MoonSharpHidden]
+    public Exception Exception { get; }
+
+    public LuaError(Exception runtimeException)
+    {
+        Exception = runtimeException;
+    }
+}
