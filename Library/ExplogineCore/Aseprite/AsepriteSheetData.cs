ce05c1a4b8c98565fad90451459a26f9185dac34
notexplosive <thenotexplosive@gmail.com>
2024-07-13 00:18:10 -0700

    aseprite import support

---
+++ Library/ExplogineCore/Aseprite/AsepriteSheetData.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ExplogineCore.Aseprite;
+
+[Serializable]
+public class AsepriteSheetData
+{
+    [JsonProperty("frames")]
+    public Dictionary<string, AsepriteFrame> Frames { get; set; } = new();
+}
