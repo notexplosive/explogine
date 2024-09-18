38bde2a1c9364d386a03743562cef8cbffec8348
notexplosive <thenotexplosive@gmail.com>
2024-09-18 08:59:30 -0700

    add new reflection api

--- Library/ExplogineCore/Reflection.cs
+++ Library/ExplogineCore/Reflection.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Diagnostics.Contracts;
+﻿using System.Diagnostics.Contracts;
 using System.Reflection;
 
 namespace ExplogineCore;
@@ -7,7 +6,7 @@ namespace ExplogineCore;
 public static class Reflection
 {
     /// <summary>
-    /// Gets static fields from type T that derive from TInterface
+    ///     Gets static fields from type T that derive from TInterface
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <typeparam name="TInterface"></typeparam>
@@ -17,9 +16,9 @@ public static class Reflection
     {
         return GetStaticFieldsThatDeriveFromType<TInterface>(typeof(T));
     }
-    
+
     /// <summary>
-    /// Gets static fields from type T that derive from TInterface
+    ///     Gets static fields from type T that derive from TInterface
     /// </summary>
     /// <typeparam name="TInterface"></typeparam>
     /// <returns></returns>
@@ -28,15 +27,16 @@ public static class Reflection
     {
         return t
             .GetFields(BindingFlags.Public | BindingFlags.Static)
-            .Where(fieldInfo => fieldInfo.FieldType.GetInterfaces().Contains(typeof(TInterface)) || fieldInfo.FieldType == typeof(TInterface))
+            .Where(fieldInfo => fieldInfo.FieldType.GetInterfaces().Contains(typeof(TInterface)) ||
+                                fieldInfo.FieldType == typeof(TInterface))
             .ToDictionary(
                 fieldInfo => fieldInfo.Name,
                 fieldInfo => (TInterface) fieldInfo.GetValue(null)!
             );
     }
-    
+
     /// <summary>
-    /// Gets static fields from type T that derive from TInterface
+    ///     Gets static fields from type T that derive from TInterface
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <typeparam name="TInterface"></typeparam>
@@ -65,12 +65,13 @@ public static class Reflection
 
             implementingTypes.AddRange(types);
         }
-        
+
         return implementingTypes;
     }
 
     [Pure]
-    public static IEnumerable<Tuple<MemberInfo, Type>> GetAllMembersInAssemblyWithAttribute<TAttribute>(Assembly assembly) where TAttribute : Attribute
+    public static IEnumerable<Tuple<MemberInfo, Type>>
+        GetAllMembersInAssemblyWithAttribute<TAttribute>(Assembly assembly) where TAttribute : Attribute
     {
         var types = assembly.GetTypes();
         var attributeType = typeof(TAttribute);
@@ -80,6 +81,17 @@ public static class Reflection
             {
                 yield return new Tuple<MemberInfo, Type>(member, type);
             }
+        }
+    }
+
+    [Pure]
+    public static IEnumerable<Tuple<MemberInfo, Type>> GetAllMembersInTypeWithAttribute<TAttribute>(Type type)
+        where TAttribute : Attribute
+    {
+        var attributeType = typeof(TAttribute);
+        foreach (var member in type.GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Where(method => Attribute.IsDefined(method, attributeType)))
+        {
+            yield return new Tuple<MemberInfo, Type>(member, type);
         }
     }
 
@@ -95,5 +107,26 @@ public static class Reflection
                 yield return type;
             }
         }
+    }
+
+    [Pure]
+    public static object? GetMemberValue(MemberInfo memberInfo, object instance)
+    {
+        if (memberInfo is FieldInfo fieldInfo)
+        {
+            return fieldInfo.GetValue(instance);
+        }
+
+        if (memberInfo is PropertyInfo propertyInfo)
+        {
+            return propertyInfo.GetValue(instance);
+        }
+
+        if (memberInfo is MethodInfo methodInfo)
+        {
+            return methodInfo.Invoke(instance, []);
+        }
+
+        throw new Exception($"Could not invoke {memberInfo} on {instance}");
     }
 }
