a79b2d890ae2688e81967db7cba94c541e141f08
notexplosive <thenotexplosive@gmail.com>
2024-11-10 17:50:40 -0800

    remove dead code

--- Library/ExplogineDesktop/DesktopPlatform.cs
+++ Library/ExplogineDesktop/DesktopPlatform.cs
@@ -1,12 +1,8 @@
-﻿using ExplogineCore;
-using ExplogineMonoGame;
+﻿using ExplogineMonoGame;
 
 namespace ExplogineDesktop;
 
 public class DesktopPlatform : IPlatformInterface
 {
     public RealWindow PlatformWindow { get; } = new DesktopWindow();
-    
-    [Obsolete("This will not work on macOS")]
-    public IFileSystem ContentFileSystem { get; } = new RealFileSystem(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content"));
 }
