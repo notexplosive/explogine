39d47f501ce5c60d1e94cb2a215e127976405986
notexplosive <thenotexplosive@gmail.com>
2025-02-01 20:20:42 -0800

    float lerp is safer

--- Library/ExTween/ExTween/FloatExtensions.cs
+++ Library/ExTween/ExTween/FloatExtensions.cs
@@ -4,7 +4,29 @@ public static class FloatExtensions
 {
     public static float Lerp(float startingValue, float targetValue, float percent)
     {
+        startingValue = Sanitized(startingValue);
+        targetValue = Sanitized(targetValue);
         return startingValue + (targetValue - startingValue) * percent;
+    }
+
+    private static float Sanitized(float value)
+    {
+        if (float.IsNaN(value))
+        {
+            return 0;
+        }
+
+        if (float.IsNegativeInfinity(value))
+        {
+            return float.MinValue;
+        }
+
+        if (float.IsInfinity(value))
+        {
+            return float.MaxValue;
+        }
+
+        return value;
     }
 }
 
