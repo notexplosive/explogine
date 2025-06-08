82beaf40ae7100d405f5d23edfb349d52de78cb0
notexplosive <thenotexplosive@gmail.com>
2025-06-08 15:15:59 -0700

    framerate counter shows more stats

--- Library/ExplogineMonoGame/Debugging/FramerateCounter.cs
+++ Library/ExplogineMonoGame/Debugging/FramerateCounter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using ExplogineMonoGame.Data;
 using ExplogineMonoGame.Rails;
 using Microsoft.Xna.Framework;
@@ -9,6 +11,7 @@ namespace ExplogineMonoGame.Debugging;
 public class FramerateCounter : IUpdateHook, IDrawHook
 {
     private DateTime _previousDraw;
+    private Queue<float> _savedDurations = new();
 
     public void Draw(Painter painter)
     {
@@ -26,6 +29,27 @@ public class FramerateCounter : IUpdateHook, IDrawHook
         }
 
         var finalString = $"{memoryUsageStat}{drawDuration:F4}s {1 / drawDuration:F0}";
+
+        _savedDurations.Enqueue(drawDuration);
+
+        while (_savedDurations.Count > 60)
+        {
+            _savedDurations.Dequeue();
+        }
+
+        var high = _savedDurations.First();
+        var low = _savedDurations.First();
+        var avg = 0f;
+        foreach (var duration in _savedDurations)
+        {
+            high = Math.Max(high, duration);
+            low = Math.Min(low, duration);
+            avg += duration;
+        }
+
+        avg /= _savedDurations.Count;
+
+        finalString += $" (avg: {1/avg:F0}, max: {1/low:F0}, min: {1/high:F0})";
 
         painter.BeginSpriteBatch();
         painter.DrawStringWithinRectangle(Client.Assets.GetFont("engine/console-font", 32), finalString,
